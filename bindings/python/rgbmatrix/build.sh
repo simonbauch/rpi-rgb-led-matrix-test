4c338e37ed56d456ed1a7b82b2e204fc9657e4c2
simonbauch <simon@simonbauch.de>
2024-06-04 00:53:53 +0200

    added buidscript for cython extension

---
+++ bindings/python/rgbmatrix/build.sh
@@ -0,0 +1,4 @@
+#Script to compile changes in the cython wrapper to the respective c++ files that get compiled into the final library
+#Requires cyton version 0.29.30
+#install with sudo pip install cython=0.29.30
+cython --cplus core.pyx graphics.pyx
