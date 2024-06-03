24761dc3ecce9ec954bdb82d8e01c7176db48d58
simonbauch <simon@simonbauch.de>
2024-06-03 21:57:54 +0200

    test

---
+++ bindings/python/samples/matrix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import time
+import sys
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
+
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 32
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+matrix = RGBMatrix(options = options)
+
+# Make image fit our screen.
+
+matrix.SetPixelsCrosshair(self, 10, 10)
+
+try:
+    print("Press CTRL-C to stop.")
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
