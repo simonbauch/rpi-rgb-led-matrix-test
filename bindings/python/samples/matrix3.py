a83b2a322e9f82b838192c9867943b8837aaecfd
simonbauch <simon@simonbauch.de>
2024-07-25 16:43:37 +0200

    added different test

---
+++ bindings/python/samples/matrix3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import time
+import sys
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
+
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 32
+options.chain_length = 14
+options.parallel = 3
+options.hardware_mapping = 'regular' # If you have an Adafruit HAT: 'adafruit-hat'
+options.gpio_slowdown = 4  
+options.multiplexing = 1
+options.pixel_mapper_config = "U-mapper"
+
+matrix = RGBMatrix(options = options)
+
+# Make image fit our screen.
+x1 = 1
+y1 = 1
+x2 = 100
+y2 = 100
+color1 = 16448250
+color2 = 16003250
+
+matrix.SetPixelstwoCrosshair(x1,y1,x2,y2,color1,color2)
+try:
+    print("Press CTRL-C to stop.")
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
