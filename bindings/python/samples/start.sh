a1e8af4eaf8c1f04ab9eab74bca60f84e166e14b
simonbauch <simon@simonbauch.de>
2024-10-10 17:33:49 +0200

    test

--- bindings/python/samples/start.sh
+++ bindings/python/samples/start.sh
@@ -2,5 +2,6 @@ cd /var/www/bindings/python/samples/
 git pull -f https://github.com/simonbauch/rpi-rgb-led-matrix-test
 while true
 do
+    cd /var/www/bindings/python/samples/
     python3 server.py
 done
